96783ad83383d747f5e35c1134ab0e4f7d0ec7e3
deronoitz <deronoitz@gmail.com>
2017-11-09 03:21:18 +0700

    add

--- js/script.js
+++ js/script.js
@@ -25,13 +25,24 @@ var searchProduct = function(){
 }
 var parallax = function(el, d){
   var item,scroll,height;
-  item = document.getElementById(el).childNodes[0];
+  if(document.getElementById(el) != null){
+    item = document.getElementById(el).childNodes[0];
+    scroll = window.scrollY;
+    height = document.body.clientHeight;
+    setTimeout(function(){
+      item.setAttribute("style", "transform: translate"+d+"(-"+scroll*.2+"px)");
+    }, 10);
+  }
 
-  scroll = window.scrollY;
-  height = document.body.clientHeight;
-  setTimeout(function(){
-    item.setAttribute("style", "transform: translate"+d+"(-"+scroll*.2+"px)");
-  }, 10);
+}
+var callModal = function(e){
+  var getModal = e.getAttribute('modal-call');
+  var modal = document.querySelector(".modal-ara#"+getModal+"");
+  var container = document.querySelectorAll('.container');
+  modal.classList.add('active');
+  container.forEach(function(i){
+    i.classList.add('blur');
+  });
 }
 if(inputSearch){
   inputSearch.addEventListener('keyup', function(){
@@ -40,4 +51,23 @@ if(inputSearch){
 }
 window.addEventListener('scroll', function(e){
   parallax("parallax", "X");
-})
+});
+
+
+var closeModal = document.querySelector('.modal-ara .close-btn > a');
+if(closeModal){
+  closeModal.addEventListener('click', function(e){
+    this.closest('.modal-ara').classList.remove('active');
+    var container = document.querySelectorAll('.container');
+    container.forEach(function(i){
+      i.classList.remove('blur');
+    });
+  })  
+}
+
+var modalHandler = document.querySelectorAll('a[modal-call]');
+modalHandler.forEach(function(item){
+  item.addEventListener('click', function(){
+    callModal(this);
+  });
+});
